22214182a96ca122c5edc15bf7e7eda879d9a1e7
Aadarsh <aadarsh0022@gmail.com>
2025-07-10 00:02:16 +0530

    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function LoginPage() {
 
         <div className="text-center">
           <p className="text-sm text-gray-600 dark:text-gray-300">
-            Don't have an account?{" "}
+            Don't have an account?
             <Link href="/sign-in" className="text-blue-600 hover:text-blue-500">
               Sign up
             </Link>
